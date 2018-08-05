fdd6ec07ad367cffdd7806eac8325133033dd342
chenxiao <xiao0413@qq.com>
2018-08-05 18:08:01 +0800

    修改组合限售玩法显示

--- src/views/foucs_event/Combination_restricted.vue
+++ src/views/foucs_event/Combination_restricted.vue
@@ -21,12 +21,14 @@
 				</template>
 			</el-table-column>
 			<el-table-column label="玩法"
-			                 prop="passType"
 			                 align="center">
+					<template slot-scope="scope">
+						{{ plays }}
+					</template>
 			</el-table-column>
 			<el-table-column label="状态"
 			                 align="center">
-											 				<template slot-scope="scope">
+				<template slot-scope="scope">
 					{{ scope.row.status | changeType}}
 				</template>
 			</el-table-column>
@@ -190,15 +192,17 @@ export default {
 			cities: cityOptions,
 			isIndeterminate: true,
 			textarea: '',
+			numPlay: [], //  存储分割的玩法
+			plays: '', //  存储删选的 玩法对应的 name
 
 		}
 	},
 	filters: {
 		type(a) {
-			return a==38 ? '竞彩足球' : '竞彩篮球'
+			return a == 38 ? '竞彩足球' : '竞彩篮球'
 		},
-		changeType(s){
-			return s==1? '有效' : '无效'
+		changeType(s) {
+			return s == 1 ? '有效' : '无效'
 		}
 	},
 	computed: {
@@ -214,11 +218,26 @@ export default {
 				page: curr,
 				pageSize: 10
 			}
-			getLotteryLimit(obj).then(res => {  //  获取渠道数
-				console.log(res)
-				if(res.data.error_code==200){
+			getLotteryLimit(obj).then(res => {  //  
+				// console.log(res)
+				if (res.data.error_code == 200) {
+					this.plays = '' //  
 					this.tableData = res.data.data.list
+					this.tableData.forEach(e => {
+						// console.log(e)
+						this.numPlay = e.passType.split(',')
+					});
+					this.numPlay.forEach(x => {
+						cityOptions.forEach(v => {
+							if (x == v.value) {
+								// this.plays.push(v.name)
+								this.plays+=v.name+','
+
+							}
+						});
+					});
 					this.total = res.data.data.total
+					console.log(this.plays)
 				}
 			})
 		},
@@ -249,6 +268,7 @@ export default {
 				if (res.status == 200) {
 					this.dialogVisible = false
 					Message.success(res.data.message)
+					this.getTable(1)
 				}
 			})
 		},
@@ -256,7 +276,7 @@ export default {
 			this.dialogVisible2 = true
 			this.onePeople = val
 		},
-		makersure() {
+		makersure() {  //  修改 组合限售的状态
 			let obj = {
 				id: this.onePeople.id,
 				status: this.radio2,
@@ -266,7 +286,8 @@ export default {
 				if (res.status == 200) {
 					this.dialogVisible2 = false
 					Message.success(res.data.message)
-				}else {
+					this.getTable(1)
+				} else {
 					Message.success(res.data.message)
 				}
 			})
