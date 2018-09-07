ac2d8ad7facdb158bf43a4f0556bf0f60bebae4c
[qinshengwei] <[694170828@qq.com]>
2018-09-07 18:14:42 +0800

    打票系统

--- src/api/ticket.js
+++ src/api/ticket.js
@@ -22,15 +22,47 @@ export function ticketPrintSearch(userInfos) {
     })
   }
 
-  export function addRoleBondPermission(userInfos) {
-    var params =new URLSearchParams();
-    params.append('value',JSON.stringify(userInfos));
-    return request.member({
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8'
-    },
-      url: '/user/addRoleBondPermission',
-      method: 'post',
-      data:JSON.stringify(userInfos)
+  //优先推送
+  export function firstPush(userinfos) {
+    return request.ticket({
+      url: '/ticketSystem/firstPush',
+      method: 'get',
+      params:userinfos
     })
   }
+
+//修改票状态
+export function updateTicketStatus(userinfos) {
+    return request.ticket({
+        url: '/ticketSystem/updateTicketStatus',
+        method: 'get',
+        params: userinfos
+    })
+}
+//修改拆包票数
+export function updateChaiPiaoCount(userinfos) {
+    return request.ticket({
+        url: '/ticketSystem/updateChaiPiaoCount',
+        method: 'get',
+        params: userinfos
+    })
+}
+
+//获取单个拆包票数
+export function getChaiPiaoCount(userinfos) {
+    return request.ticket({
+        url: '/ticketSystem/getChaiPiaoCount',
+        method: 'get',
+        params: userinfos
+    })
+}
+
+// //修改单个拆包票数
+// export function updateChaiPiaoCount(userinfos) {
+//     return request.ticket({
+//         url: '/ticketSystem/updateChaiPiaoCount',
+//         method: 'get',
+//         params: userinfos
+//     })
+// }
+
