04b61cb0bc984c63cc41103423e10d7a69f5c671
zing_queen <1175214756@qq.com>
2018-11-09 17:03:31 +0800

    市场加白修改

--- src/views/swiperWall/activate_Wall.vue
+++ src/views/swiperWall/activate_Wall.vue
@@ -17,14 +17,14 @@
                 label="注册时间"
                 align="center">
                 <template slot-scope="scope">
-                    {{scope.row.registerDateTime | time}}
+                    {{scope.row.REGISTER_DATE_TIME | time}}
                 </template>
             </el-table-column>
             <el-table-column
                 label="最后登陆时间"
                 align="center">
                 <template slot-scope="scope">
-                    {{scope.row.lastLoginDateTime | time}}
+                    {{scope.row.LAST_LOGIN_DATE_TIME | time}}
                 </template>
             </el-table-column>
             <el-table-column
@@ -51,140 +51,198 @@
 </template>
 
 <script>
-import { findAllMember,memberToWrite,getHistoryClient } from '@/api/customer'
-import { Message, MessageBox } from 'element-ui'
+import {
+  findAllMember,
+  memberToWrite,
+  getHistoryClient,
+  findAllMemberByLoginAcc
+} from "@/api/customer";
+import { Message, MessageBox } from "element-ui";
 export default {
-    data(){
-        return {
-            tableData:[],
-            account:'',
-            name:'',
-            idcard: '',
-            mobile: '',
-            endTime:'',
-            mobile:'',
-            page: 1,
-            pageSize: 20,
-            startTime:'',
-            username:'',
-            type:'',
-            totalList: 0,
-        }
-    },
-    filters:{
-        time(a){
-            if(a != null && a != ''){
-                let date = new Date(a);
-                let y = date.getFullYear();
-                let MM = date.getMonth() + 1;
-                MM = MM < 10 ? ('0' + MM) : MM;
-                let d = date.getDate();
-                d = d < 10 ? ('0' + d) : d;
-                let h = date.getHours();
-                h = h < 10 ? ('0' + h) : h;
-                let m = date.getMinutes();
-                m = m < 10 ? ('0' + m) : m;
-                let s = date.getSeconds();
-                s = s < 10 ? ('0' + s) : s;
-                return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s;
-            }
-        },
-        mtype(a){
-              return a == 1 ?"否" :  "是"
-        },
-        port(m){
-              return m == 1 ? "否" : "是"
-        }
-    },
-    created(){
-				// this.gettablelist()
-				this.getTodayDate()
-    },
-    methods:{
-			// 默认当天
-        getTodayDate(){
-            let date = new Date()
-            let y = date.getFullYear();
-            let m = date.getMonth() + 1;
-            m = m < 10 ? ('0' + m) : m;
-            let d = date.getDate();
-            d = d < 10 ? ('0' + d) : d;
-            this.startTime =  y + '-' + m + '-' + d +' '+ '00:00:00';
-            this.endTime = y + '-' + m + '-' + d +' '+ '23:59:59';
-            this.gettablelist()
-        },
-        //获取表格数据
-        gettablelist(){
-            let obj = {
-                account: this.account,
-                end_time: this.endTime,
-                mobile: this.mobile,
-                page: this.page,
-                pageSize: this.pageSize,
-                start_time: this.startTime,
-                username: this.username,
-                realName: this.name,
-                type: this.type,
-                identifyId: this.idcard,
-                mobile: this.mobile,
-            }
-            findAllMember(obj).then(res => {
-							if(res.data.error_code === 200){
-								this.tableData = res.data.data.list
-                this.totalList = res.data.data.total
-							}else{
-								this.$message.error(res.data.message)
-							}
-            }).catch(error => {
-                Message.error(error)
-            })
-        },
+  data() {
+    return {
+      tableData: [],
+      account: "",
+      name: "",
+      idcard: "",
+      mobile: "",
+      endTime: "",
+      mobile: "",
+      page: 1,
+      pageSize: 20,
+      startTime: "",
+      username: "",
+      type: "",
+      totalList: 0,
 
-        //加白
-        addwhite(data){
-            this.account = data.ACCOUNT;
-            this.type = 1
-            memberToWrite(this.account,this.type).then(res => {
-                if (res.data.error_code === 200) {
-										this.account = ''
-										this.gettablelist()
-										Message.success('加白成功')
-                } else {
-                    Message.error(res.data.message)
-                }
-            })
-        },
-        //取消加白
-        deletewhite(data){
-            this.account = data.ACCOUNT;
-            this.type = 2
-            memberToWrite(this.account,this.type).then(res => {
-                if (res.data.error_code === 200) {
-                    
-										this.account = ''
-										this.gettablelist()
-										Message.success('取消加白成功')
-                } else {
-                    Message.error(res.data.message)
-                }
-            })
-        },
-         //翻页
-        handleCurrentChange(num){
-						this.page = num;
-						this.gettablelist();
-        },
-        //改变页面大小
-        handleSizeChange(num){
-						this.pageSize = num;
-						this.gettablelist();
-        },
+      today: ""
+    };
+  },
+  filters: {
+    time(a) {
+      if (a != null && a != "") {
+        let date = new Date(a);
+        let y = date.getFullYear();
+        let MM = date.getMonth() + 1;
+        MM = MM < 10 ? "0" + MM : MM;
+        let d = date.getDate();
+        d = d < 10 ? "0" + d : d;
+        let h = date.getHours();
+        h = h < 10 ? "0" + h : h;
+        let m = date.getMinutes();
+        m = m < 10 ? "0" + m : m;
+        let s = date.getSeconds();
+        s = s < 10 ? "0" + s : s;
+        return y + "-" + MM + "-" + d + " " + h + ":" + m + ":" + s;
+      }
+    },
+    mtype(a) {
+      return a == 1 ? "否" : "是";
+    },
+    port(m) {
+      return m == 1 ? "否" : "是";
     }
-}
+  },
+  created() {
+    this.gettablelist();
+    this.getTodayDate();
+  },
+  methods: {
+    //获取当前日期
+    getTodayDate() {
+      let date = new Date();
+      let y = date.getFullYear();
+      let m = date.getMonth() + 1;
+      m = m < 10 ? "0" + m : m;
+      let d = date.getDate();
+      d = d < 10 ? "0" + d : d;
+      //   let h = date.getHours();
+      //   h = h < 10 ? "0" + h : h;
+      //   let min = date.getMinutes();
+      //   min = min < 10 ? "0" + min : min;
+      //   let s = date.getSeconds();
+      //   s = s < 10 ? "0" + s : s;
+      this.today = y + "-" + m + "-" + d;
+    },
+    //时间戳转日期
+    time(a) {
+      if (a != null && a != "") {
+        let date = new Date(a);
+        let y = date.getFullYear();
+        let MM = date.getMonth() + 1;
+        MM = MM < 10 ? "0" + MM : MM;
+        let d = date.getDate();
+        d = d < 10 ? "0" + d : d;
+        // let h = date.getHours();
+        // h = h < 10 ? "0" + h : h;
+        // let m = date.getMinutes();
+        // m = m < 10 ? "0" + m : m;
+        // let s = date.getSeconds();
+        // s = s < 10 ? "0" + s : s;
+        return y + "-" + MM + "-" + d;
+      }
+    },
+    //获取表格数据
+    // gettablelist() {
+    //   let obj = {
+    //     account: this.account,
+    //     end_time: this.endTime,
+    //     mobile: this.mobile,
+    //     page: this.page,
+    //     pageSize: this.pageSize,
+    //     start_time: this.startTime,
+    //     username: this.username,
+    //     realName: this.name,
+    //     type: this.type,
+    //     identifyId: this.idcard,
+    //     mobile: this.mobile
+    //   };
+    //   findAllMember(obj)
+    //     .then(res => {
+    //       if (res.data.error_code === 200) {
+    //         this.tableData = res.data.data.list;
+    //         this.totalList = res.data.data.total;
+    //       } else {
+    //         this.$message.error(res.data.message);
+    //       }
+    //     })
+    //     .catch(error => {
+    //       Message.error(error);
+    //     });
+    // },
+
+    gettablelist() {
+      let obj = {
+        account: "",
+        identifyId: "",
+        loginAccount: this.$store.state.user.name,
+        mobile: "",
+        page: this.page,
+        pageSize: this.pageSize,
+        realName: "",
+        username: ""
+      };
+      findAllMemberByLoginAcc(obj).then(res => {
+        if (res.data.error_code == 200) {
+          this.totalList = res.data.data.total;
+          this.tableData = res.data.data.list.filter(e => {
+            // console.log(this.time(e.REGISTER_DATE_TIME));
+            return this.today === this.time(e.REGISTER_DATE_TIME);
+          });
+          this.totalList = this.tableData.length;
+          this.$message.success(res.data.message);
+        } else {
+          this.totalList = "";
+          this.tableData = [];
+          this.$message.error(res.data.message);
+        }
+      });
+    },
+
+    //加白
+    addwhite(data) {
+      this.account = data.ACCOUNT;
+      this.type = 1;
+      memberToWrite(this.account, this.type).then(res => {
+        if (res.data.error_code === 200) {
+          this.account = "";
+          this.gettablelist();
+          Message.success("加白成功");
+        } else {
+          Message.error(res.data.message);
+        }
+      });
+    },
+    //取消加白
+    deletewhite(data) {
+      this.account = data.ACCOUNT;
+      this.type = 2;
+      memberToWrite(this.account, this.type).then(res => {
+        if (res.data.error_code === 200) {
+          this.account = "";
+          this.gettablelist();
+          Message.success("取消加白成功");
+        } else {
+          Message.error(res.data.message);
+        }
+      });
+    },
+    //翻页
+    handleCurrentChange(num) {
+      this.page = num;
+      this.gettablelist();
+    },
+    //改变页面大小
+    handleSizeChange(num) {
+      this.pageSize = num;
+      this.gettablelist();
+    }
+  }
+};
 </script>
 
 <style>
-.statistics{
-    padding: 10px 20px
+.statistics {
+  padding: 10px 20px;
 }
 </style>
