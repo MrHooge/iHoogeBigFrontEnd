fab2f015f171a3eb4848265e45dc262223fded4b
zing_queen <1175214756@qq.com>
2018-12-05 11:39:43 +0800

    添加图标样式

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,29 +1,38 @@
 <template>
   <div class="menu-wrapper">
-        <!-- <el-menu class="sidebar-el-menu" :collapse="collapse" background-color="#324157" -->
-        <el-menu class="sidebar-el-menu" background-color="#324157"
-                text-color="#bfcbd9" active-text-color="#fff">
-    <template v-for="item in routes">
-      <el-submenu :index="item.modelNme" :key="item.modelNme">
-        <template slot="title">
-          <span v-if="item.modelNme" slot="title">{{item.modelNme}}</span>
-        </template>
-        <template v-for="child in item.childList">
-          <router-link :to="child.model_url" :key="child.model_name">
-            <el-menu-item :index="child.model_name" style="background-color:red;">
-              <span v-if="child.model_name" slot="title">{{child.model_name}}</span>
-            </el-menu-item>
-          </router-link>
-        </template>
-      </el-submenu>
-    </template>
+    <!-- <el-menu class="sidebar-el-menu" :collapse="collapse" background-color="#324157" -->
+    <el-menu
+      class="sidebar-el-menu"
+      background-color="#324157"
+      text-color="#bfcbd9"
+      active-text-color="#fff"
+    >
+      <template v-for="item in routes">
+        <el-submenu :index="item.modelNme" :key="item.modelNme">
+          <template slot="title">
+            <span class="nav" v-if="item.modelNme" slot="title">
+              <i class="iconfont" style="color:#fff" :class="item.icon"></i>
+              {{item.modelNme}}
+            </span>
+          </template>
+
+          <template v-for="child in item.childList">
+            <router-link :to="child.model_url" :key="child.model_name">
+              <el-menu-item :index="child.model_name" style="background-color:red;">
+                <span v-if="child.model_name" slot="title">{{child.model_name}}</span>
+              </el-menu-item>
+            </router-link>
+          </template>
+        </el-submenu>
+      </template>
     </el-menu>
   </div>
 </template>
 
 <script>
+import "./ifont/iconfont.css";
 export default {
-  name: 'SidebarItem',
+  name: "SidebarItem",
   props: {
     routes: {
       type: Array
@@ -33,18 +42,50 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      iconArr: [
+        "icon-shouye",
+        "icon-kefu",
+        "icon-browser",
+        "icon-wxbgongju",
+        "icon-eraser",
+        "icon-shenfenzheng",
+        "icon-dingdan",
+        "icon-gonglve",
+        "icon-hongbao",
+        "icon-iconfontxingxing",
+        "icon-liwu",
+        "icon-ziyouanpai",
+        "icon-xinxi",
+        "icon-set",
+        "icon-jinnang",
+        "icon-xinyongqia",
+        "icon-xiangbao"
+      ]
+    };
+  },
   created() {
+    this.routes.forEach((e, index) => {
+      e.icon = this.iconArr[index];
+    });
   },
   methods: {
     hasOneShowingChildren(children) {
       const showingChildren = children.filter(item => {
-        return !item.hidden
-      })
+        return !item.hidden;
+      });
       if (showingChildren.length === 1) {
-        return true
+        return true;
       }
-      return false
+      return false;
     }
   }
-}
+};
 </script>
+<style scoped>
+.el-submenu__title {
+  vertical-align: text-top;
+}
+</style>
+
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,32 +1,52 @@
 <template>
   <div class="login-container">
-    <el-form class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left">
+    <el-form
+      class="login-form"
+      autocomplete="on"
+      :model="loginForm"
+      :rules="loginRules"
+      ref="loginForm"
+      label-position="left"
+    >
       <h3 class="title">麒云运营管理系统</h3>
       <el-form-item prop="username">
         <span class="svg-container svg-container_login">
-          <svg-icon icon-class="user" />
+          <svg-icon icon-class="user"/>
         </span>
-        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="username" clearable />
+        <el-input
+          name="username"
+          type="text"
+          v-model="loginForm.username"
+          autocomplete="on"
+          placeholder="username"
+          clearable
+        />
       </el-form-item>
       <el-form-item prop="password">
         <span class="svg-container">
           <svg-icon icon-class="password"></svg-icon>
         </span>
-        <el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
-          placeholder="password" clearable></el-input>
-          <span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye" /></span>
+        <el-input
+          name="password"
+          :type="pwdType"
+          @keyup.enter.native="handleLogin"
+          v-model="loginForm.password"
+          autocomplete="on"
+          placeholder="password"
+          clearable
+        ></el-input>
+        <span class="show-pwd" @click="showPwd">
+          <svg-icon icon-class="eye"/>
+        </span>
       </el-form-item>
       <el-form-item>
         <!-- <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin"> -->
-
-        <el-button type="primary" style="width:100%;" @click.native.prevent="handleLogin">
-          登 录
-        </el-button>
+        <el-button type="primary" style="width:100%;" @click.native.prevent="handleLogin">登 录</el-button>
       </el-form-item>
       <!-- <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
         <span> password: admin</span>
-      </div> -->
+      </div>-->
     </el-form>
   </div>
 </template>
@@ -90,6 +110,7 @@ export default {
             });
         } else {
           this.loading = false;
+          this.$message.error(res.data);
           return false;
         }
       });
