9bc90e7790d3204a8bdf3d7d32eefb9a16afb7a8
MrHooge <mrhooge@outlook.com>
2019-01-22 17:56:20 +0800

    测试api

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,21 +2,15 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 const QYAPI = {
-  // member: "'https://member.api.qiyun88.cn'",
-  // user: "'https://user.api.qiyun88.cn'",
-  // infos: "'https://infos.api.qiyun88.cn'",
-  // pay: "'https://pay.api.qiyun88.cn'",
-  // ticket: "'https://ticket.api.qiyun88.cn'",
-  // lottery: "'https://lottery.api.qiyun88.cn'"
-  member: "'http://192.168.1.37:10010'",
-    user: "'http://192.168.1.37:10010'",
-    infos: "'http://192.168.1.37:10030'",
-    pay: "'http://192.168.1.37:10050'",
-    ticket: "'http://192.168.1.37:10070'",
-    lottery: "'http://192.168.1.37:10090'"
+    member: "'http://memberapi.qiyun88.cn'",
+    user: "'http://userapi.qiyun88.cn'",
+    infos: "'http://infosapi.qiyun88.cn'",
+    pay: "'http://payapi.qiyun88.cn'",
+    ticket: "'http://ticketapi.qiyun88.cn'",
+    lottery: "'http://lotteryapi.qiyun88.cn'"
 }
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_API: QYAPI,
+    NODE_ENV: '"development"',
+    BASE_API: QYAPI,
 })
